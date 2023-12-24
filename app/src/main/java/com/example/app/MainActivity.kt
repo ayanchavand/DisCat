182acfc51fe8d324100f4a6317a244c12e39229c
ayanchavand <ayan.chavand15@gmail.com>
2023-12-24 14:07:53 +0530

    Added better documentation

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -21,55 +21,81 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Log message for debugging
         Log.d("LOG", "Hello")
+
+        // Standard onCreate setup
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image: ImageView =  findViewById(R.id.image)
+        // View references
+        val image: ImageView = findViewById(R.id.image)
         val submitCat: Button = findViewById(R.id.submitCat)
         val submitDog: Button = findViewById(R.id.submitDog)
         val apiToggle: Switch = findViewById(R.id.apiToggle)
         val apiText: TextView = findViewById(R.id.apiText)
 
-        fun showToast( message: String) {
+        // Function to display Toast messages
+        fun showToast(message: String) {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
 
+        // Click listener for the "Submit Cat" button
         submitCat.setOnClickListener {
+            // Coroutine launched on the main thread
             GlobalScope.launch(Dispatchers.Main) {
+                // Clear the image
                 Glide.with(this@MainActivity).clear(image)
+
                 if (apiToggle.isChecked()) {
+                    // API toggle is checked, load cat image from "cataas.com"
                     apiText.text = "API: cataas.com"
                     val imageUrl = "https://cataas.com/cat?timestamp=${System.currentTimeMillis()}"
                     Glide.with(this@MainActivity).load(imageUrl).into(image)
                 } else {
+                    // API toggle is not checked, load cat image from "thecatapi.com"
                     apiText.text = "API: thecatapi.com"
                     try {
+                        // Make an asynchronous HTTP request for cat image
                         val response = httpReq("null", "https://api.thecatapi.com/v1/images/search")
                         Log.d("LOG!", response)
+
+                        // Parse the JSON response to get the image URL
                         val imageUrl = jsonParserCat(response)
                         Log.d("LOG!", imageUrl)
+
+                        // Load the cat image using Glide
                         Glide.with(this@MainActivity).load(imageUrl).into(image)
                     } catch (e: Exception) {
                         // Handle exceptions, e.g., network errors
                         Log.e("LOG!", "Error fetching data", e)
+
+                        // Show a Toast message for error notification
                         showToast("Something went wrong: Error fetching data")
                     }
                 }
             }
         }
 
+        // Click listener for the "Submit Dog" button
         submitDog.setOnClickListener {
+            // Coroutine launched on the main thread
             GlobalScope.launch(Dispatchers.Main) {
+                // Update the API text
                 apiText.text = "API: dog.ceo"
                 try {
+                    // Make an asynchronous HTTP request for dog image
                     val response = httpReq("", "https://dog.ceo/api/breeds/image/random")
                     val imageUrl = jsonParserDog(response)
                     Log.d("LOG!", imageUrl)
+
+                    // Load the dog image using Glide
                     Glide.with(this@MainActivity).load(imageUrl).into(image)
                 } catch (e: Exception) {
                     // Handle exceptions, e.g., network errors
                     Log.e("LOG!", "Error fetching data", e)
+
+                    // Show a Toast message for error notification
                     showToast("Something went wrong: Error fetching data")
                 }
             }
@@ -77,25 +103,30 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
+// Function to make an asynchronous HTTP request
 private suspend fun httpReq(apiKey: String, url: String): String = withContext(Dispatchers.IO) {
     val client = OkHttpClient()
     val request = Request.Builder().url(url).build()
 
     return@withContext try {
+        // Execute the HTTP request synchronously and get the response
         client.newCall(request).execute().use { response ->
             if (response.isSuccessful) {
+                // Return the response body as a string
                 response.body!!.string()
             } else {
+                // Throw an exception for unexpected HTTP codes
                 throw IOException("Unexpected HTTP code: ${response.code}")
             }
         }
     } catch (e: IOException) {
+        // Throw an exception for errors during the HTTP request
         throw IOException("Error during HTTP request", e)
     }
 }
 
-private fun jsonParserCat(responseString: String): String{
+// Function to parse JSON response for cat image
+private fun jsonParserCat(responseString: String): String {
     val gson = Gson()
     val catDataArray = gson.fromJson(responseString, Array<CatData>::class.java)
 
@@ -111,12 +142,13 @@ private fun jsonParserCat(responseString: String): String{
     }
 }
 
-private fun jsonParserDog(responseString: String): String{
+// Function to parse JSON response for dog image
+private fun jsonParserDog(responseString: String): String {
     val gson = Gson()
     val dogData = gson.fromJson(responseString, DogData::class.java)
 
-    return dogData.message;
+    // Return the dog image URL
+    return dogData.message
 }
-
 
 
